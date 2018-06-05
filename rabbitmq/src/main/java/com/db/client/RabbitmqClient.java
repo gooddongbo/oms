4c8811a32cf5ea820a70d8c172ea9aeee81710fc
dongbo <dongbo@globalegrow.com>
2018-06-05 10:17:20 +0800

    12345

--- rabbitmq/src/main/java/com/db/client/RabbitmqClient.java
+++ rabbitmq/src/main/java/com/db/client/RabbitmqClient.java
@@ -1,9 +1,12 @@
 package com.db.client;
 
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class RabbitmqClient {
@@ -32,8 +35,25 @@ public class RabbitmqClient {
         }
         //创建 channel实例
         channel = connection.createChannel();
-        channel.exchangeDeclare("exchangeName","direct",true);
-        channel.queueDeclare("queueName", false, false, false, null);
+        byte[] messageBodyBytes = "Hello, world!".getBytes();
+        //设置队列所有消息的过期时间
+        Map<String, Object> args = new HashMap<String,Object>();
+        args.put("x-expires",10000);
+        channel.queueDeclare("queueName", false, false, false, args);
+        channel.queueBind("queueName", "exchangeName", "queueName");
+        channel.basicPublish("exchangeName","queueName",null,messageBodyBytes);
+
+        //将队列和exchange绑定：队列名称，exchangeName名称，routing-key
+//        channel.queueBind("queueName", "exchangeName", "queueName");
+
+        //发送一条直能存活10s的消息到队列（单独给一条消息设置存活时间）
+//        AMQP.BasicProperties properties = new AMQP.BasicProperties.Builder().expiration("10000").build();
+//        channel.basicPublish("exchangeName","queueName",properties,messageBodyBytes);
+
+
+
+//        channel.exchangeDeclare("exchangeName","direct",true);
+//        channel.queueDeclare("queueName", false, false, false, null);
     }
 
     public static void main(String[] args) {
